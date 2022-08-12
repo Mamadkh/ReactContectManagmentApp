0b8803ff6328a96be480c0dad7d7bc89ead2b5f3
Mohammadreza khorsand <105081547+Mamadkh@users.noreply.github.com>
2022-08-11 21:34:47 -0400

    main

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div className="App">
       <h3>Contact Managment Application</h3>
+      <span className='fa fa-address-book'></span>
+      <button className='btn btn-info'><i className='fa fa-facebook'> Click Me!</i></button>
     </div>
   );
 }
